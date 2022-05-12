9defedfd2fa625ca377c23859714c3bebc62cec7
daraholic <1112dara@gmail.com>
2022-05-13 00:27:35 +0800

    changed edu text size

--- front/about.php
+++ front/about.php
@@ -16,29 +16,3 @@
         </div>
     </div>
 </div>
-
-
-<!-- original -->
-
-<!-- <div class="col-12 mb-1 mx-5" id="about">
-<div class="container">
-    <div class="row">
-        <div class="card-group">
-            <div class="card">
-                <img class="img-thumbnail" src="./img/<?=$Photo->find(['sh' => 1])['img'];?>" alt="<?=$Photo->find(['sh' => 1])['text'];?>" width="288" height="364">
-            </div>
-            <div class="card">
-                <div class="card-body text-VeryPeri">
-                    <h4><i class="fa fa-certificate fa-fw"></i>About</h4>
-                    <h2><?=$Info->find(1)['text'];?></h2>
-                    <p><i class="fa fa-briefcase"></i>&nbsp;<?=$Info->find(2)['text'];?></p>
-                    <p><i class="fa fa-home"></i>&nbsp;<?=$Info->find(3)['text'];?></p>
-                    <p><i class="fa fa-envelope"></i>&nbsp;<?=$Info->find(4)['text'];?></p>
-                    <p><i class="fab fa-github"></i>&nbsp;<a target="_blank" href="<?=$Info->find(5)['text'];?>">
-                    <?=$Info->find(5)['text'];?></p></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div> -->
--- front/edu.php
+++ front/edu.php
@@ -1,24 +1,24 @@
 <div class="col-12 text-grey transbox p-3 mb-1 bg-body rounded">         
     <div class="container" id="educ">
-        <h4><i class="fa fa-asterisk fa-fw"></i>Education</h4>
+        <h3><i class="fa fa-asterisk fa-fw"></i>Education</h3>
             <div class="container">
-                <h2>Workforce Development Agency, Ministry of Labor </h2>
+                <h4>Workforce Development Agency, Ministry of Labor </h4>
                 <h6><i class="fa fa-calendar fa-fw"></i>Sept. 2021 – Mar. 2022</h6>
                 <p>learning web programming</p>
             </div>
             <hr>
             <div class="container">
-                <h5>Graduated from Korean Language Center at Sungkyunkwan University</h5>
+                <h4>Graduated from Korean Language Center at Sungkyunkwan University</h4>
                 <h6><i class="fa fa-calendar fa-fw"></i>May 2019 – Jan. 2020</h6>
-                <p>Passed threshold of Regular Korean Program and got the certificate of completion</p>
+                <h5>Passed threshold of Regular Korean Program and got the certificate of completion</h5>
             </div>
             <hr>
             <div class="container">
-                <h5>B.A. degree of Japanese Language and Literature, Tamkang University</h5>
+                <h4>B.A. degree of Japanese Language and Literature, Tamkang University</h4>
                 <h6><i class="fa fa-calendar fa-fw"></i>Sept. 2009 – Jan. 2014</h6>
                 <p>Bachelor Degree</p>
                 <hr>
-                <h5>Finished the course of Akita International University (AIU), Japan</h5>
+                <h4>Finished the course of Akita International University (AIU), Japan</h4>
             </div>
             <div class="container">
                 <h6><i class="fa fa-calendar fa-fw"></i>Jan. 2012</h6>
